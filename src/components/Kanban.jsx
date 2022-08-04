9497573d5629fdf80a7854122b748ca0d363ac84
erikgab01 <erikgab01@gmail.com>
2022-08-04 19:21:30 +0500

    Reworked drag and drop using react-beautiful-dnd

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -1,13 +1,28 @@
+import { nanoid } from "nanoid";
 import React, { useState } from "react";
 import TaskCreator from "./TaskCreator";
 import TasksBoard from "./TasksBoard";
 
 export default function Kanban() {
     const [groups, setGroups] = useState([
-        { title: "Бэклог", color: "#6A6E6B", tasks: ["Сделать домашку", "Test"] },
-        { title: "В процессе", color: "#EB6B50", tasks: ["123", " наш корСломатьабль"] },
-        { title: "Завершено", color: "#56BB62", tasks: ["bgfd", "Сломать наш корабль"] },
-        { title: "Отложено", color: "#82569C", tasks: ["yrtyrtyrty", "5354353453"] },
+        {
+            title: "Бэклог",
+            color: "#6A6E6B",
+            tasks: [
+                { id: nanoid(), content: "Сделать домашку" },
+                { id: nanoid(), content: "Сделать домашку" },
+            ],
+        },
+        {
+            title: "В процессе",
+            color: "#EB6B50",
+            tasks: [
+                { id: nanoid(), content: "Сделать домашку" },
+                { id: nanoid(), content: "Сделать домашку" },
+            ],
+        },
+        { title: "Завершено", color: "#56BB62", tasks: [{ id: nanoid(), content: "Сделать домашку" }] },
+        { title: "Отложено", color: "#82569C", tasks: [{ id: nanoid(), content: "Сделать домашку" }] },
     ]);
 
     return (
--- src/components/TasksBoard.jsx
+++ src/components/TasksBoard.jsx
@@ -1,76 +1,103 @@
 import React, { useState, useRef } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { hexToRgb } from "../utility";
 
 export default function TasksBoard({ groups, setGroups }) {
-    const [dragging, setDragging] = useState(false);
-    // useRef to maintain their values between rerenders
-    const dragItem = useRef();
-    const dragItemNode = useRef();
-    function handleDragStart(e, item) {
-        dragItemNode.current = e.target;
-        dragItemNode.current.addEventListener("dragend", handleDragEnd);
-        dragItem.current = item;
+    function onDragEnd(result) {
+        const { source, destination } = result;
 
-        setTimeout(() => {
-            setDragging(true);
-        }, 0);
-    }
-    function handleDragEnter(e, targetItem) {
-        if (dragItem.current.groupI !== targetItem.groupI || dragItem.current.taskI !== targetItem.taskI) {
-            setGroups((oldList) => {
-                let newList = JSON.parse(JSON.stringify(oldList));
-                newList[targetItem.groupI].tasks.splice(
-                    targetItem.taskI,
-                    0,
-                    newList[dragItem.current.groupI].tasks.splice(dragItem.current.taskI, 1)[0]
-                );
-                dragItem.current = targetItem;
-                return newList;
-            });
+        // dropped outside the list
+        if (!destination) {
+            return;
+        }
+        const sInd = +source.droppableId;
+        const dInd = +destination.droppableId;
+
+        if (sInd === dInd) {
+            const items = reorder(groups[sInd].tasks, source.index, destination.index);
+            const newGroups = JSON.parse(JSON.stringify(groups));
+            newGroups[sInd].tasks = items;
+            setGroups(newGroups);
+        } else {
+            const result = move(groups[sInd].tasks, groups[dInd].tasks, source, destination);
+            const newGroups = JSON.parse(JSON.stringify(groups));
+            newGroups[sInd].tasks = result[sInd];
+            newGroups[dInd].tasks = result[dInd];
+
+            setGroups(newGroups);
         }
     }
-    function handleDragEnd(e) {
-        setDragging(false);
-        dragItem.current = null;
-        dragItemNode.current.removeEventListener("dragend", handleDragEnd);
-        dragItemNode.current = null;
+    function reorder(list, startIndex, endIndex) {
+        const result = [...list];
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+
+        return result;
+    }
+
+    /**
+     * Moves an item from one list to another list.
+     */
+    function move(source, destination, droppableSource, droppableDestination) {
+        const sourceClone = Array.from(source);
+        const destClone = Array.from(destination);
+        const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+        destClone.splice(droppableDestination.index, 0, removed);
+
+        const result = {};
+        result[droppableSource.droppableId] = sourceClone;
+        result[droppableDestination.droppableId] = destClone;
+
+        return result;
     }
     return (
-        <section className="flex gap-12 mt-8">
-            {groups.map((group, groupI) => {
-                const { r, g, b } = hexToRgb(group.color);
-                const textColor = r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#ffffff";
-                return (
-                    <div
-                        key={groupI}
-                        onDragEnter={
-                            dragging && !group.tasks.length
-                                ? (e) => handleDragEnter(e, { groupI, taskI: 0 })
-                                : null
-                        }
-                        className="flex flex-col gap-5"
-                    >
-                        <h6
-                            style={{ background: group.color, color: textColor }}
-                            className="py-3 px-4 self-start rounded-md"
-                        >
-                            {group.title}
-                        </h6>
-                        {group.tasks.map((task, taskI) => (
-                            <div
-                                draggable
-                                key={task}
-                                onDragStart={(e) => handleDragStart(e, { groupI, taskI })}
-                                onDragEnter={dragging ? (e) => handleDragEnter(e, { groupI, taskI }) : null}
-                                style={{ borderColor: group.color }}
-                                className="bg-white border-l-4 p-4 rounded-md"
+        <DragDropContext onDragEnd={onDragEnd}>
+            <section className="flex gap-12 mt-8">
+                {groups.map((group, groupI) => {
+                    const { r, g, b } = hexToRgb(group.color);
+                    const textColor = r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#ffffff";
+                    return (
+                        <div className="flex flex-col gap-5">
+                            <h6
+                                style={{ background: group.color, color: textColor }}
+                                className="py-3 px-4 self-start rounded-md"
                             >
-                                <p>{task}</p>
-                            </div>
-                        ))}
-                    </div>
-                );
-            })}
-        </section>
+                                {group.title}
+                            </h6>
+                            <Droppable key={groupI} droppableId={`${groupI}`}>
+                                {(provided, snapshot) => (
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.droppableProps}
+                                        className="flex flex-col gap-5"
+                                    >
+                                        {group.tasks.map((task, taskI) => (
+                                            <Draggable key={task.id} draggableId={task.id} index={taskI}>
+                                                {(provided, snapshot) => (
+                                                    <div
+                                                        ref={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                        style={{
+                                                            borderColor: group.color,
+                                                            ...provided.draggableProps.style,
+                                                        }}
+                                                        className="bg-white border-l-4 p-4 rounded-md"
+                                                    >
+                                                        <p>{task.content}</p>
+                                                    </div>
+                                                )}
+                                            </Draggable>
+                                        ))}
+                                        {provided.placeholder}
+                                    </div>
+                                )}
+                            </Droppable>
+                        </div>
+                    );
+                })}
+            </section>
+        </DragDropContext>
     );
 }

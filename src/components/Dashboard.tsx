f96186addd2f409585c200b449938023a7ba2729
erikgab01 <erikgab01@gmail.com>
2023-01-20 15:40:40 +0500

    Added loading for dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -10,9 +10,13 @@ import useContextMenu from "../hooks/useContextMenu";
 import ContextMenu from "./utility/ContextMenu";
 import { createNewKanban, deleteKanban, updateKanbanInfo } from "../api/kanbanService";
 import { KanbanDoc } from "../types";
+import KanbanSkeleton from "./utility/KanbanSkeleton";
 
 export default function Dashboard() {
     // TODO: loading
+    // TODO: decompose
+    const [isLoadingHostKanbans, setIsLoadingHostKanbans] = useState(true);
+    const [isLoadingCollabKanbans, setIsLoadingCollabKanbans] = useState(true);
     const [isShowCreateModal, setIsShowCreateModal] = useState(false);
     const [isShowEditModal, setIsShowEditModal] = useState(false);
     const [isShowConfirmationModal, setIsShowConfirmationModal] = useState(false);
@@ -50,9 +54,11 @@ export default function Dashboard() {
             where("collaborators", "array-contains", auth.currentUser?.uid)
         );
         const unsubscribe1 = onSnapshot(q1, (querySnapshot) => {
+            setIsLoadingHostKanbans(false);
             setKanbanHostList(querySnapshot.docs as unknown as KanbanDoc[]);
         });
         const unsubscribe2 = onSnapshot(q2, (querySnapshot) => {
+            setIsLoadingCollabKanbans(false);
             setKanbanCollabList(querySnapshot.docs as unknown as KanbanDoc[]);
         });
         return () => {
@@ -66,56 +72,73 @@ export default function Dashboard() {
             <h1 className="font-medium text-lg text-center">Добро пожаловать</h1>
             <h4 className="font-medium text-center">Ваши доски</h4>
             <div className="grid gap-4 grid-cols-3">
-                {kanbanHostList.map((kanban) => (
-                    <Link
-                        key={kanban.id}
-                        to={`/kanban/${kanban.id}`}
-                        className="w-60 h-28 p-6 text-center bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-                        onContextMenu={(e) => {
-                            e.preventDefault();
-                            setClicked(true);
-                            setPoints({
-                                x: e.pageX,
-                                y: e.pageY,
-                            });
-                            setContextMenuTarget(kanban);
-                        }}
-                    >
-                        <h5 className="mb-2 text-xl truncate font-bold tracking-tight text-gray-900 dark:text-white">
-                            {kanban.data().name}
-                        </h5>
-                        <p className="font-normal truncate text-sm text-gray-700 dark:text-gray-400">
-                            {kanban.data().description}
-                        </p>
-                    </Link>
-                ))}
-
-                <button
-                    className="w-60 h-28 p-6 text-2xl bg-white border border-dashed border-gray-600 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-                    onClick={() => setIsShowCreateModal(true)}
-                >
-                    <FontAwesomeIcon icon={["fas", "plus"]} />
-                </button>
+                {isLoadingHostKanbans ? (
+                    <>
+                        <KanbanSkeleton />
+                        <KanbanSkeleton />
+                        <KanbanSkeleton />
+                    </>
+                ) : (
+                    <>
+                        {kanbanHostList.map((kanban) => (
+                            <Link
+                                key={kanban.id}
+                                to={`/kanban/${kanban.id}`}
+                                className="w-60 h-28 p-6 text-center bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                                onContextMenu={(e) => {
+                                    e.preventDefault();
+                                    setClicked(true);
+                                    setPoints({
+                                        x: e.pageX,
+                                        y: e.pageY,
+                                    });
+                                    setContextMenuTarget(kanban);
+                                }}
+                            >
+                                <h5 className="mb-2 text-xl truncate font-bold tracking-tight text-gray-900 dark:text-white">
+                                    {kanban.data().name}
+                                </h5>
+                                <p className="font-normal truncate text-sm text-gray-700 dark:text-gray-400">
+                                    {kanban.data().description}
+                                </p>
+                            </Link>
+                        ))}
+                        <button
+                            className="w-60 h-28 p-6 text-2xl bg-white border border-dashed border-gray-600 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                            onClick={() => setIsShowCreateModal(true)}
+                        >
+                            <FontAwesomeIcon icon={["fas", "plus"]} />
+                        </button>
+                    </>
+                )}
             </div>
             <h4 className="font-medium text-cente mt-5">Доски, к которым вам дали доступ</h4>
-            {kanbanCollabList.length === 0 && (
+            {kanbanCollabList.length === 0 && !isLoadingCollabKanbans && (
                 <p>Попросите владельца доски дать вам доступ, после этого вы увидите её тут</p>
             )}
             <div className="grid gap-4 grid-cols-3">
-                {kanbanCollabList.map((kanban) => (
-                    <Link
-                        key={kanban.id}
-                        to={`/kanban/${kanban.id}`}
-                        className="w-60 h-28 p-6 text-center bg-white border border-yellow-400 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-                    >
-                        <h5 className="mb-2 text-xl truncate font-bold tracking-tight text-gray-900 dark:text-white">
-                            {kanban.data().name}
-                        </h5>
-                        <p className="font-normal truncate text-sm text-gray-700 dark:text-gray-400">
-                            {kanban.data().description}
-                        </p>
-                    </Link>
-                ))}
+                {isLoadingCollabKanbans ? (
+                    <>
+                        <KanbanSkeleton />
+                        <KanbanSkeleton />
+                        <KanbanSkeleton />
+                    </>
+                ) : (
+                    kanbanCollabList.map((kanban) => (
+                        <Link
+                            key={kanban.id}
+                            to={`/kanban/${kanban.id}`}
+                            className="w-60 h-28 p-6 text-center bg-white border border-yellow-400 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                        >
+                            <h5 className="mb-2 text-xl truncate font-bold tracking-tight text-gray-900 dark:text-white">
+                                {kanban.data().name}
+                            </h5>
+                            <p className="font-normal truncate text-sm text-gray-700 dark:text-gray-400">
+                                {kanban.data().description}
+                            </p>
+                        </Link>
+                    ))
+                )}
             </div>
             {/* Create new kanban modal */}
             <Modal isShow={isShowCreateModal} setIsShow={setIsShowCreateModal}>
---
+++ src/components/utility/KanbanSkeleton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function KanbanSkeleton() {
+    return (
+        <div className="w-60 h-28 mx-auto border-2 rounded-lg bg-white shadow-md">
+            <div className="flex flex-row items-center justify-center h-full space-x-5 animate-pulse">
+                <div className="flex flex-col space-y-3">
+                    <div className="w-32 h-4 bg-gray-300 rounded-md"></div>
+                    <div className="w-16 h-3 bg-gray-300 rounded-md self-center"></div>
+                </div>
+            </div>
+        </div>
+    );
+}

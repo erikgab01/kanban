d5c859bfa64e824dd48db656b09a205b9aab8495
erikgab01 <erikgab01@gmail.com>
2023-02-12 01:54:25 +0500

    Moved styles from root div to layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout() {
         navigate("/");
     }
     return (
-        <>
+        <div className="flex flex-col h-screen">
             <nav className="text-white bg-sky-600 py-4">
                 <div className="container mx-auto flex items-center justify-between">
                     <h1 className="text-2xl font-medium">
@@ -28,6 +28,6 @@ export default function Layout() {
                 <ToastContainer position="bottom-right" newestOnTop />
             </nav>
             <Outlet />
-        </>
+        </div>
     );
 }
